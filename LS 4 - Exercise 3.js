a8f9ec230dae7bb7588e962c42e460b092bd6b87
Denny <denton.cordell@sbcglobal.net>
2019-05-08 15:15:14 -0500

    Create LS 4 - Exercise 3.js
    Yeah, hit the brick wall here.

---
+++ LS 4 - Exercise 3.js
@@ -0,0 +1,20 @@
+let full_name = {
+    first_name1: 'George',
+    last_name1: 'Washington',
+    first_name2: 'Andrew',
+    last_name2: 'Jackson',
+    first_name3: 'John',
+    last_name3: 'Adams',
+    first_name4: 'John',
+    last_name4: 'Kennedy',
+}
+
+for ( let last in full_name ) {
+  console.log(full_name[last]);
+    let counter = 0
+    for (counter = 0; counter < full_name.length - 1; counter++){
+    }
+}
+
+  //if (object.hasOwnPrope let(last)) {full_name
+//}
