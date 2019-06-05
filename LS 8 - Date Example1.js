71e08e6630eb9887972944f007a414a6b030dd86
Denny <denton.cordell@sbcglobal.net>
2019-06-05 18:36:00 -0500

    LS 8 -  Interactive Calendar
    Found an Interactive Calendar for prroject!!

---
+++ LS 8 - Date Example1.js
@@ -0,0 +1,3 @@
+let my_date = '2017-04-06';
+add_days_to_date(my_date, 4);
+console.log(format_as_string(my_date, 'Month Day, Year'));
---
+++ LS 8 - Date Example2.js
@@ -0,0 +1,3 @@
+let d = new MyDate('2017-04-06');
+d.addDays(4);
+console.log(d.toString('Month Day, Year'));
